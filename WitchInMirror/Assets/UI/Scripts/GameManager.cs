e3ff537f7da2392e821c9fcbe7a98fda9a25c696
RUNETIA <runetia123@gmail.com>
2023-03-22 18:19:10 +0900

    UI

--- WitchInMirror/Assets/UI/Scripts/GameManager.cs
+++ WitchInMirror/Assets/UI/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> listGUIScenes;
+    public enum E_SCENE { PLAY, TITLE, GAMEOVER, MAX }
+    public E_SCENE curScene;
+
     public GameObject pausePanel;
     public GameObject settingPanel;
     private bool isPause = false;
@@ -14,13 +18,56 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    GameManager gameManager;
+
+    public void Initialize(GameManager gameManager)
+    {
+        gameObject.SetActive(true);
+        SetGUIState(curScene);
+        this.gameManager = gameManager;
+    }
+
+    public void ShowGUIState(E_SCENE scene)
+    {
+        for (E_SCENE idx = E_SCENE.PLAY; idx < E_SCENE.MAX; idx++)
+        {
+            if (idx == scene)
+                listGUIScenes[(int)idx].SetActive(true);
+            else
+                listGUIScenes[(int)idx].SetActive(false);
+        }
+    }
+
+    public void SetGUIState(E_SCENE scene)
+    {
+        switch (scene)
+        {
+            case E_SCENE.PLAY:
+                Time.timeScale = 1;
+                break;
+            case E_SCENE.TITLE:
+                Time.timeScale = 0;
+                break;
+            case E_SCENE.GAMEOVER:
+                Time.timeScale = 0;
+                break;
+        }
+        ShowGUIState(scene);
+        curScene = scene;
+    }
+
+    public void EventSenceChange(int idx)
+    {
+        SetGUIState((E_SCENE)idx);
     }
 
     public void SetPause()
@@ -35,8 +82,8 @@ public class GameManager : MonoBehaviour
 
     public void PauseContinue()
     {
-        if (isPause) 
-        { 
+        if (isPause)
+        {
             isPause = false;
             Time.timeScale = 1f;
             pausePanel.gameObject.SetActive(false);
@@ -61,5 +108,10 @@ public class GameManager : MonoBehaviour
             Time.timeScale = 1f;
             settingPanel.gameObject.SetActive(false);
         }
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
     }
 }
